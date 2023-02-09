18249fc46e54381e98d1c684fab0c4604eb7d813
Kennan <kennanhunter5@gmail.com>
2023-02-09 00:35:49 -0500

    power animation

--- src/lib/components/Repl.svelte
+++ src/lib/components/Repl.svelte
@@ -1,20 +1,91 @@
 <script lang="ts" context="module">
-	import { z } from "zod";
-	import { EditorView, basicSetup } from "codemirror";
-	import { javascript } from "@codemirror/lang-javascript";
-	import { onMount } from "svelte";
-	import { powerStore } from "$lib/stores/power";
-	import * as ts from "typescript";
 	import { currentStateStore } from "$lib/stores/currentState";
+	import { powerStore } from "$lib/stores/power";
+	import { javascript } from "@codemirror/lang-javascript";
+	import { basicSetup, EditorView } from "codemirror";
+	import { onMount } from "svelte";
+	import * as ts from "typescript";
+	import { z, ZodError } from "zod";
 
 	let el: Element;
 	export let source: string = `// All values will be cropped to be within 0 and 1.0
-export default ((): number => {
-    return 1.0
-})
+export default (seconds: number): number => {
+	return Math.abs(Math.sin(Math.PI * seconds));
+};
 `;
 
+	let startTime: Date;
+
+	const elapsedTime = (): number =>
+		(new Date().getTime() - startTime.getTime()) / 1000;
+
+	export type GetPower = () => Promise<number | undefined>;
+	export const getPower: GetPower = async () => {
+		const transpiledCode = ts.transpile(source, {
+			target: ts.ScriptTarget.ESNext,
+			module: ts.ModuleKind.ESNext,
+		});
+
+		if (!transpiledCode) {
+			console.error("Fuck");
+			powerStore.set({
+				status: "error",
+				reason: "No code transpiled",
+			});
+		}
+
+		const dataUri =
+			"data:text/javascript;charset=utf-8," +
+			encodeURIComponent(transpiledCode);
+
+		const module = await import(/* @vite-ignore */ dataUri);
+
+		const getRawPower = await z
+			.function()
+			.args(z.number())
+			.returns(z.number())
+			.parseAsync(module.default)
+			.catch(() => {
+				console.log("Code does not follow schema");
+
+				powerStore.set({
+					status: "error",
+					reason: "Code doe does not follow schema",
+				});
+			});
+
+		if (!getRawPower) return;
+
+		const power = await z
+			.number()
+			.min(0, "Number cannot be less than 0")
+			.max(1, "Number cannot be more than 1")
+			.parseAsync(getRawPower(elapsedTime()))
+			.catch((err) => {
+				console.log(err ?? "Code did not return a number");
+
+				powerStore.set({
+					status: "error",
+					reason: (err as ZodError).message ?? "Code did not return a number",
+				});
+			});
+
+		return power ?? undefined;
+	};
+
+	export const runLight = async () => {
+		currentStateStore.set("RUNNING");
+	};
+
+	export const stopLight = async () => {
+		currentStateStore.set("IDLE");
+	};
+</script>
+
+<script lang="ts">
 	onMount(() => {
+		startTime = new Date();
+
 		let editor = new EditorView({
 			extensions: [
 				basicSetup,
@@ -27,67 +98,6 @@ export default ((): number => {
 			doc: source,
 		});
 	});
-
-	export type GetPower = () => Promise<number | undefined>;
-	export const getPower: GetPower = async () => {
-		console.log("running");
-
-		const transpiledCode = ts.transpile(
-			`// All values will be cropped to be within 0 and 1.0
-export default (): number => {
-return 1.0;
-};
-`,
-			{ target: ts.ScriptTarget.ESNext, module: ts.ModuleKind.ESNext },
-		);
-
-		if (!transpiledCode) {
-			console.error("Fuck");
-			powerStore.set({
-				status: "error",
-				reason: "No code transpiled",
-			});
-		}
-
-		console.dir({
-			transpiledCode,
-		});
-
-		const dataUri =
-			"data:text/javascript;charset=utf-8," +
-			encodeURIComponent(transpiledCode);
-		const module = await import(dataUri);
-		console.dir(module);
-
-		if (!module.default) {
-			powerStore.set({
-				status: "error",
-				reason: "No Module exported",
-			});
-		}
-
-		const rawPower = module.default();
-
-		const power = await z
-			.number()
-			.min(0, "Number cannot be less than 0")
-			.max(1, "Number cannot be more than 1")
-			.parseAsync(rawPower)
-			.catch((err) => {
-				console.log(err ?? "Code did not return a number");
-
-				powerStore.set({
-					status: "error",
-					reason: err ?? "Code did not return a number",
-				});
-			});
-
-		return power ?? undefined;
-	};
-
-	export const run = async () => {
-		currentStateStore.set("RUNNING");
-	};
 </script>
 
 <div class="bg-stone-900 w-full">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,43 @@
 <script lang="ts">
-	import Repl, { getPower, run } from "$lib/components/Repl.svelte";
+	import Repl, {
+		getPower,
+		runLight,
+		stopLight,
+	} from "$lib/components/Repl.svelte";
 	import { currentStateStore } from "$lib/stores/currentState";
 	import { powerStore } from "$lib/stores/power";
 	import { onMount } from "svelte";
 
-	let startTime = new Date();
-
-	/**
-	 * Returns seconds
-	 */
-	const elapsedTime = (): number =>
-		(new Date().getTime() - startTime.getTime()) / 1000;
-
 	onMount(() => {
-		setInterval(async () => {
+		const interval = setInterval(async () => {
 			if ($currentStateStore === "IDLE") return;
+
+			if (!getPower) return;
 
 			if ($currentStateStore === "RUNNING") {
 				powerStore.set({
 					status: "successful",
 					power:
 						(await getPower()) ??
-						($powerStore.status === "successful" ? $powerStore.power : 0),
+						($powerStore.status === "successful" ? $powerStore.power : -1),
 				});
 			}
 		}, 10);
+
+		return () => clearInterval(interval);
+	});
+
+	const toHSLLightness = () => {
+		if ($powerStore.status === "error") return 20;
+
+		return $powerStore.power * 50 + 20;
+	};
+
+	let hslLightness: number;
+
+	powerStore.subscribe(() => {
+		hslLightness = toHSLLightness();
+		console.log(hslLightness);
 	});
 </script>
 
@@ -40,15 +53,29 @@
 	<section
 		class="grid place-content-center bg-stone-800 text-white rounded-lg p-4"
 	>
-		<h1>Light Preview</h1>
+		<div class="w-m-1/2">
+			<h1>Light Preview</h1>
+
+			<svg>
+				<rect
+					width="150"
+					height="150"
+					fill={`hsl(204 100% ${hslLightness.toFixed(0)}%)`}
+				/>
+			</svg>
+		</div>
 
 		{#if $powerStore.status === "error"}
 			<pre class="border-black text-red">{$powerStore.reason}</pre>
 		{:else if $powerStore.status === "successful"}
-			<pre class="border-black text-red">{$powerStore.power}</pre>
+			<pre class="border-black text-red">Power: {$powerStore.power.toFixed(
+					3,
+				)}</pre>
 		{/if}
 
-		<button on:click={run}>Run</button>
+		<button on:click={$currentStateStore === "IDLE" ? runLight : stopLight}
+			>{$currentStateStore === "IDLE" ? "run" : "stop"}</button
+		>
 	</section>
 
 	<section
